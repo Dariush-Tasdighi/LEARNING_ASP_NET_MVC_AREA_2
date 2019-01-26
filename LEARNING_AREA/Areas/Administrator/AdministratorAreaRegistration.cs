4276be7d09f8992c8483b350774fda597033d4f6
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2019-01-26 15:23:35 +0330

    Add project files.

---
+++ LEARNING_AREA/Areas/Administrator/AdministratorAreaRegistration.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+
+namespace LEARNING_AREA.Areas.Administrator
+{
+	public class AdministratorAreaRegistration : AreaRegistration
+	{
+		public AdministratorAreaRegistration() : base()
+		{
+		}
+
+		public override string AreaName
+		{
+			get
+			{
+				return "Administrator";
+			}
+		}
+
+		public override void RegisterArea(AreaRegistrationContext context)
+		{
+			//context.MapRoute(
+			//	"Administrator_Default",
+			//	"Administrator/{controller}/{action}/{id}",
+			//	new { action = "Index", id = UrlParameter.Optional }
+			//);
+
+			context.MapRoute(
+				"Administrator_Default",
+				"Administrator/{controller}/{action}/{id}",
+				new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+			);
+		}
+	}
+}
